ccea5ab92a01262d859b36c297924d37a6ce6a24
Nilton Pontes <59576458+niltoneapontes@users.noreply.github.com>
2020-08-15 22:44:45 -0300

    Adicionando Build

---
+++ build/precache-manifest.c010c621e77657c650dd5e7604c1c917.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80f5f9b9ea3cb9be427da9b6a1794a59",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76504d692af4c8187756",
+    "url": "/static/css/main.2b24906a.chunk.css"
+  },
+  {
+    "revision": "6edb467e19d2896c75e3",
+    "url": "/static/js/2.458f7acd.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.458f7acd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76504d692af4c8187756",
+    "url": "/static/js/main.d96c9814.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "37f88678bb571adbf3212ca39d8e09a7",
+    "url": "/static/media/home.37f88678.svg"
+  },
+  {
+    "revision": "6e31edff1e2cb5a2420637ac397f5311",
+    "url": "/static/media/list.6e31edff.svg"
+  },
+  {
+    "revision": "f2f703a1091cf5e90a2954ed5fecd96d",
+    "url": "/static/media/reunion.f2f703a1.svg"
+  },
+  {
+    "revision": "0aca04b3f0fe383fc1721ec4c7d25eeb",
+    "url": "/static/media/schedule.0aca04b3.svg"
+  },
+  {
+    "revision": "62ed0e29bea932b70d96ad35f275f663",
+    "url": "/static/media/users.62ed0e29.svg"
+  }
+]);
